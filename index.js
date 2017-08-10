6b32835bebc1d60c64fd86c70887410d2cadad65
Rangel <rangel.varnier@gmail.com>
2017-08-09 23:49:46 -0300

    add redux, para gerenciar stores

--- index.js
+++ index.js
@@ -1,24 +1,10 @@
 const counter = (state = 0, action) => {
     switch (action.type) {
-      case 'INCREMENT':
-        return state + 1;
-      case 'DECREMENT':
-        return state - 1;
-      default:
-        return state;
+        case 'INCREMENT':
+            return state + 1;
+        case 'DECREMENT':
+            return state - 1;
+        default:
+            return state;
     }
-  }
-
-expect(counter(0, {type: 'INCREMENT'})).toEqual(1);
-
-expect(counter(1, {type: 'INCREMENT'})).toEqual(2);
-
-expect(counter(2, {type: 'DECREMENT'})).toEqual(1);
-
-expect(counter(1, {type: 'DECREMENT'})).toEqual(0);
-
-expect(counter(1, {type: 'OTHER'})).toEqual(1);
-
-expect(counter(undefined, {type: 'OTHER'})).toEqual(0);
-
-console.log("tests ok!")
+}
---
+++ test.js
@@ -0,0 +1,13 @@
+expect(counter(0, {type: 'INCREMENT'})).toEqual(1);
+
+expect(counter(1, {type: 'INCREMENT'})).toEqual(2);
+
+expect(counter(2, {type: 'DECREMENT'})).toEqual(1);
+
+expect(counter(1, {type: 'DECREMENT'})).toEqual(0);
+
+expect(counter(1, {type: 'OTHER'})).toEqual(1);
+
+expect(counter(undefined, {type: 'OTHER'})).toEqual(0);
+
+console.log("tests ok!")
