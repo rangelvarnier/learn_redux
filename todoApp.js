37a8103d6da3f7a132bc32262acf49c2d0cdfde4
Rangel <rangel.varnier@gmail.com>
2017-08-16 22:49:45 -0300

    implementação do combineReducers do zero

--- todoApp.js
+++ todoApp.js
@@ -7,5 +7,25 @@ const visibilityFilter = (state = 'SHOW_ALL', action) => {
     }
 }
 
-const {combineReducers} = Redux;
-const todoApp = combineReducers({todos, visibilityFilter});
+//const {combineReducers} = Redux;
+
+const combineReducers = reducers => {
+    return (state = {}, action) => {
+
+        return Object
+            .keys(reducers)
+            .reduce((nextState, key) => {
+                    nextState[key] = reducers[key](
+                        state[key], 
+                        action
+                    );
+                    return nextState;
+                }, {} 
+            );
+    };
+};
+
+const todoApp = combineReducers({
+    todos,
+    visibilityFilter
+});
